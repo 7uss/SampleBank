fcb4081f97192c1ceb001dcfbd7cde423b849d32
7uss <husseiny.a.z@gmail.com>
2022-04-23 17:48:33 +0300

    edit: rename UserBeneficiaryAccounts table to match name in database
    the table name was changed to match the name in the database for error free referencing of the relation.
    old name: UserBeneficiaryAccounts
    new name: AdvanceUserBankAccount (same as in database)

---
+++ Data/Migrations/20220416002355_renameUserBankAccountsTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleBank.Data.Migrations
+{
+    public partial class renameUserBankAccountsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "UserBeneficiaryAccounts",
+                newName: "AdvanceUserBankAccount");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "AdvanceUserBankAccount",
+                newName: "UserBeneficiaryAccounts");
+        }
+    }
+}
